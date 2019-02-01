faaced81ab281ae8a5808b8c734b12e9cced311c
jacobmsilva <jacob_silva@my.uri.edu>
2019-02-01 10:41:11 -0500

    Added given code
    yéet

--- GameOfLife.py
+++ GameOfLife.py
@@ -5,3 +5,23 @@
 #Any live cell with two or three live neighbors lives on to the next generation
 #Any lvie cell with more than three live neighbors dies, as if by overpopulation
 #any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction
+import os
+import time
+
+def display_array(ar):
+    "Clear the screen, display the contents of an array, wait for 1sec"
+    os.system('clear')
+    #grab the rows
+    rows = len(ar)
+
+    if rows == 0:
+        raise ValueError("Array contains no data")
+    #grab the columns - indices start at 0
+    cols = len(ar[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            #no carriage return, space separated
+            print(ar[i][j],end=' ')
+        print()
+    time.sleep(1)
