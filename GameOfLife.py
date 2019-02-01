844dca811731a6d3432f7268c8e47a7b421d2f3c
jacobmsilva <jacob_silva@my.uri.edu>
2019-02-01 10:34:47 -0500

    Starting

---
+++ GameOfLife.py
@@ -0,0 +1,7 @@
+#Rules to Game of Life
+#Any live cell with fewer than two live neighbors dies,
+#as if caused by underpopulation
+
+#Any live cell with two or three live neighbors lives on to the next generation
+#Any lvie cell with more than three live neighbors dies, as if by overpopulation
+#any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction
