b6bc2038f1d8eb4798d9c0a40605bb12442a6754
jacobmsilva <jacob_silva@my.uri.edu>
2019-02-03 02:47:07 -0500

    Game of Life
    All that's left is reworking the neighbor checking

--- GameOfLife.py
+++ GameOfLife.py
@@ -23,6 +23,8 @@ def RandomlyFillArray(ar):
         for x in range(cols):
             if (random.randint(0,1)==1):
                 ar[i][x]= '*'
+            else:
+                ar[i][x]="_"
     return ar
 
 def display_array(ar):
@@ -43,14 +45,124 @@ def display_array(ar):
         print()
     time.sleep(1)
     return ar
+def check_neighbors(ar):
+    rows = len(ar)
+    cols = len(ar[0])
+    NextGeneration = [['',' ',' ',' ',' ' ],
+          [' ',' ',' ',' ',' ' ],
+          [' ',' ',' ',' ',' ' ],
+          [' ',' ',' ',' ',' ' ],
+          [' ',' ',' ',' ',' ' ]]
+    #this counter will calculate the surrounding neighbors
+    #if this counter is equal to 2 or 3 then the cell will live
+    # otherwise if its greater than 3 or less than 2 it will die
+    for i in range(rows-1):
+        Neighbor = 0
+        for x in range(cols-1):
+            if(ar[i][x] == "*"):
+                #this will now check to the left and the right
+                #Checking all the corners
+                if(x == cols):
+                    if(i == 0):
+                        #top right corner
+                        if(ar[i][x] == ar[i+1][x]):
+                            Neighbor = Neighbor + 1
+                        if(ar[i][x]==ar[i][x-1]):
+                            Neighbor = Neighbor + 1
+                    elif(i == rows ):
+                        #bottom right corner
+                        if(ar[i][x] == ar[i-1][x]):
+                            Neighbor = Neighbor + 1
+                        if(ar[i][x]==ar[i][x-1]):
+                            Neighbor = Neighbor + 1
+                elif(x == 0):
+                    if(i==0):
+                        #top left
+                        if(ar[i][x]==ar[i][x+1]):
+                            Neighbor = Neighbor + 1
+                        if(ar[i][x] == ar[i+1][x]):
+                            Neighbor = Neighbor + 1
+                    elif(i==rows):
+                        #bottom left corner
+                        if(ar[i][x]==ar[i-1][x]):
+                            Neighbor=Neighbor+1
+                        if(ar[i][x]==ar[i][x+1]):
+                            Neighbor = Neighbor + 1
+                ##checking everything besides corners
+                ##starting off with top row checking left right and down
+                elif(i==0 and x !=0 and x != cols ):
+                    if(ar[i][x]==ar[i+1][x]):
+                        Neighbor=Neighbor+1
+                    if(ar[i][x]==ar[i][x+1]):
+                        Neighbor=Neighbor+1
+                    if(ar[i][x]==ar[i][x-1]):
+                        Neighbor=Neighbor+1
+                elif(i==rows and x!=0 and x !=cols):
+                    ##bottom row checking left right and up
+                    if(ar[i][x]==ar[i][x+1]):
+                        Neighbor=Neighbor+1
+                    if(ar[i][x]==ar[i][x-1]):
+                        Neighbor=Neighbor+1
+                    if(ar[i][x]==ar[i-1][x]):
+                        Neighbor=Neighbor+1
+                elif(i != rows and i !=0 and x != cols  and x != 0):
+                    ##now we can check in every direction (up down left and right)
+                    if(ar[i][x]==ar[i][x+1]):
+                        Neighbor=Neighbor+1
+                    if(ar[i][x]==ar[i][x-1]):
+                        Neighbor=Neighbor+1
+                    if(ar[i][x]==ar[i-1][x]):
+                        Neighbor=Neighbor+1
+                    if(ar[i][x]==ar[i+1][x]):
+                        Neighbor=Neighbor+1
+                
+                    
+                if (Neighbor == 2 or Neighbor == 3):
+                    NextGeneration[i][x] = "*"
+    #fill in the blanks
+    for i in range(rows):
+        for x in range(cols):
+            if(NextGeneration[i][x] !="*"):
+                NextGeneration[i][x] ="_"
+    return NextGeneration
+
+
+def GameOfLife(ar,x):
+    #begin recursive calls
+    previousBoardState = ar
+    nextGeneration = check_neighbors(ar)
+    #check if this generation is all dead now
+    EmptyMap = True
+    rows = len(ar)
+    cols = len(ar[0])
+    for i in range(rows):
+        for j in range(cols):
+            if(ar[i][j] =="*"):
+                EmptyMap = False
+    if (EmptyMap == True):
+        print("Game has ended")
+    else:
+        x=x+1
+        print("Generation: ",x)
+        display_array(nextGeneration)
+        
+    if(previousBoardState != nextGeneration):
+       GameOfLife(nextGeneration,x)
+    
 def main():
-    ar = [[' ',' ',' ',' ',' ' ],
+    
+    ar = [['',' ',' ',' ',' ' ],
           [' ',' ',' ',' ',' ' ],
           [' ',' ',' ',' ',' ' ],
           [' ',' ',' ',' ',' ' ],
           [' ',' ',' ',' ',' ' ]]
     ar= RandomlyFillArray(ar)
+    print("Generation: 1")
+    x = 1
     display_array(ar)
+    GameOfLife(ar,x)
+
+    
 
 
 # add live cells that fulfil any of the rules to a list
