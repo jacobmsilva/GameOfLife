544938a52f55a64b588532a99c9ac2a50c115d29
jacobmsilva <jacob_silva@my.uri.edu>
2019-02-03 00:15:39 -0500

    Updated some Python
    added functioning randomizing system and array

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,3 +1,7 @@
+#Names of members
+#Fehmina
+#Victoria
+#Jacob S.
 #Rules to Game of Life
 #Any live cell with fewer than two live neighbors dies,
 #as if caused by underpopulation
@@ -7,6 +11,19 @@
 #any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction
 import os
 import time
+import random
+#starts at zero so really rows = 5 and cols = 5
+#this is just easier to read the loops this way
+
+
+def RandomlyFillArray(ar):
+    rows = len(ar)
+    cols = len(ar[0])
+    for i in range(rows):
+        for x in range(cols):
+            if (random.randint(0,1)==1):
+                ar[i][x]= '*'
+    return ar
 
 def display_array(ar):
     "Clear the screen, display the contents of an array, wait for 1sec"
@@ -25,3 +42,16 @@ def display_array(ar):
             print(ar[i][j],end=' ')
         print()
     time.sleep(1)
+    return ar
+def main():
+    ar = [[' ',' ',' ',' ',' ' ],
+          [' ',' ',' ',' ',' ' ],
+          [' ',' ',' ',' ',' ' ],
+          [' ',' ',' ',' ',' ' ],
+          [' ',' ',' ',' ',' ' ]]
+    ar= RandomlyFillArray(ar)
+    display_array(ar)
+
+
+# add live cells that fulfil any of the rules to a list
+#so they can be compared to with others later
